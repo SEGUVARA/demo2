c3b8f65acf7a6fa01216978cfcf2087f2b5fc837
SEGUVARA <146626561+SEGUVARA@users.noreply.github.com>
2024-09-24 13:39:32 +0530

    add files

---
+++ guvi/kd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+thangam class
+{
+    public static void main(string[]args);
+    int a,b,c;
+    scanner sc=new scanner;
+    system.out.println("sankar is a father of gautham");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=a+b;
+    system.out.println("gautham "+c)
+}
